883cf2b3ed1af5c6c2bc671258c02b3f678b3cbb
Manichand <mani.chand01@gmail.com>
2024-01-16 22:12:07 +0530

    Hello world completed

---
+++ hello-world/src/main.rs
@@ -0,0 +1,9 @@
+use axum::{routing::get,Router};
+#[tokio::main]
+async fn main(){
+    let app = Router::new().route("/",get(|| async {"Hello world !"}));
+    axum::server::bind(&"0.0.0.0:8000".parse().unwrap())
+                    .serve(app.into_make_service())
+                    .await
+                    .unwrap();
+}
