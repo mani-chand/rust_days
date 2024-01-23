31a3146df8b52cbf6bcea3d6e4952221fe070eba
Manichand Kondapaka <65899331+mani-chand@users.noreply.github.com>
2024-01-23 19:25:04 +0530

    Feature : MongoDB database connection for adding notes to database.

--- notes/src/main.rs
+++ notes/src/main.rs
@@ -1,25 +1,51 @@
-use axum::{routing::post,Router, Json};
-use serde::Deserialize;
+use axum::{extract::Json, routing::{post,get}, Router};
+use mongodb::{options::ClientOptions, Client, bson::doc};
 
-#[derive(Deserialize)]
-struct Note{
-    title:String,
-    content:String
+#[derive(serde::Deserialize,Debug)]
+struct Note {
+    title: String,
+    content: String,
+}
+
+async fn create_note(Json(note): Json<Note>) {
+    let title = note.title;
+    let content = note.content;
+    
+    // Assuming you have a MongoDB collection named "notes"
+    let client_options = ClientOptions::parse("mongodb+srv://manichand:root@cluster0.oll6q.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
+    let client = Client::with_options(client_options).unwrap();
+    let db = client.database("Rust_notes");
+    let collection = db.collection("Notes");
+
+    // Create a BSON document to insert into the MongoDB collection
+    let document = doc! {
+        "title": title,
+        "content": content,
+    };
+
+    // Insert the document into the collection
+    collection.insert_one(document, None).await.unwrap();
+
+    // Optionally, you might want to return a response indicating success
+    // For simplicity, this example returns a plain string
+}
+
+
+async fn get_all_notes()->Json<Note>{
+    let client_options = ClientOptions::parse("mongodb+srv://manichand:root@cluster0.oll6q.mongodb.net/?retryWrites=true&w=majority").await.unwrap();
+    let client = Client::with_options(client_options).unwrap();
+    let db = client.database("Rust_notes");
+    let collection = db.collection::<Note>("Notes");
+    let cursor = collection.find(None, None).await.unwrap();
+    // println!("{:?}",cursor);
+    // Json(cursor)
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/",post(create_note));
-    axum::Server::bind(&"0.0.0.0:5000".parse().unwrap())
-                .serve(app.into_make_service())
-                .await
-                .unwrap();
-}
-
-async fn create_note(Json(note):Json<Note>)->String{
-    let mut notes = vec![];
-    let title = note.title;
-    let content = note.content;
-    notes.push(Note{title,content});
-    format!("Received title: {} and content: {}", notes[0].title, notes[0].content)
+    let app = Router::new().route("/create_note", post(create_note)).route("/",get(get_all_notes));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
